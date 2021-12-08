7e53f24d84f27ab7cb3ad6d7ba9bc8c064ab614e
yuvaraj457 <86401198+yuvaraj457@users.noreply.github.com>
2021-12-08 19:35:07 +0530

    Delete Redux_Sample_Application3 directory

--- Redux_Sample_Application3/sample_application2/src/Store/index.js
+++
@@ -1,6 +0,0 @@
-import {applyMiddleware, createStore} from 'redux'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import { todoReducer } from '../Reducer/todoReducer'
-
-export const store = createStore(todoReducer, composeWithDevTools(applyMiddleware(thunk))) 
